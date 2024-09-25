db29bba9aaf312f7b0b0da91cb18f9cf7d053b33
Michael Bleigh <bleigh@google.com>
2024-09-25 14:16:50 -0700

    Adds LICENSE and license headers.

--- js/src/parse.ts
+++ js/src/parse.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { parse } from "yaml";
 import { DataArgument, Document, MediaPart, Message, Part, PromptMetadata } from "./types";
 
@@ -78,7 +94,7 @@ export function toMessages<ModelConfig = Record<string, any>>(
   return insertHistory(messages, data?.history);
 }
 
-function insertHistory(messages: Message[], history?: Message[]) {
+function insertHistory(messages: Message[], history: Message[] = []) {
   if (!history || messages.find((m) => m.metadata?.purpose === "history")) return messages;
   if (messages.at(-1)?.role === "user") {
     return [...messages.slice(0, -1), ...history, messages.at(-1)];
